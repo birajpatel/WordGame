fbf18727213f281c3e0e388be392a358b79faf31
Biraj Patel <biraj.d.patel@gmail.com>
2016-09-14 01:45:56 +0530

    ApplicatonComponent setup

--- app/src/main/java/com/birin/wordgame/presentation/BaseFragment.java
+++ app/src/main/java/com/birin/wordgame/presentation/BaseFragment.java
@@ -2,10 +2,15 @@ package com.birin.wordgame.presentation;
 
 import android.support.v4.app.Fragment;
 
+import com.birin.wordgame.presentation.di.ApplicationComponent;
+
 /**
  Created by Biraj on 9/14/16.
  */
 public class BaseFragment extends Fragment {
 
+    public ApplicationComponent getApplicationComponent() {
+        return ((MainApplication) getContext().getApplicationContext()).getApplicationComponent();
+    }
 
 }
--- app/src/main/java/com/birin/wordgame/presentation/MainApplication.java
+++ app/src/main/java/com/birin/wordgame/presentation/MainApplication.java
@@ -2,9 +2,36 @@ package com.birin.wordgame.presentation;
 
 import android.app.Application;
 
+import com.birin.wordgame.presentation.di.ApplicationComponent;
+import com.birin.wordgame.presentation.di.ApplicationModule;
+import com.birin.wordgame.presentation.di.DaggerApplicationComponent;
+import com.birin.wordgame.presentation.di.RepositoryModule;
+import com.birin.wordgame.presentation.di.threading.SchedulerModule;
+import com.birin.wordgame.presentation.di.timer.ClockModule;
+
 /**
  Created by Biraj on 9/14/16.
  */
 public class MainApplication extends Application {
 
+    private ApplicationComponent applicationComponent;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        applicationComponent = loadComponentBuilder();
+    }
+
+    public ApplicationComponent getApplicationComponent() {
+        return this.applicationComponent;
+    }
+
+    protected ApplicationComponent loadComponentBuilder() {
+        return DaggerApplicationComponent.builder()
+                                         .applicationModule(new ApplicationModule(this))
+                                         .clockModule(new ClockModule())
+                                         .repositoryModule(new RepositoryModule())
+                                         .schedulerModule(new SchedulerModule())
+                                         .build();
+    }
 }
