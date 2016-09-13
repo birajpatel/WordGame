69a9c8aacb5b990846d45ee76e88acfd2dc2b932
Biraj Patel <biraj.d.patel@gmail.com>
2016-09-13 21:17:38 +0530

    Implemented ScoreManager

--- app/src/main/java/com/birin/wordgame/core/score/ScoreManager.java
+++ app/src/main/java/com/birin/wordgame/core/score/ScoreManager.java
@@ -2,26 +2,29 @@ package com.birin.wordgame.core.score;
 
 
 import rx.Observable;
+import rx.subjects.BehaviorSubject;
 
 /**
  Created by Biraj on 9/13/16.
  */
 public class ScoreManager {
 
-    public void incrementScore(final int incrementBy) {
+    private BehaviorSubject<Integer> score = BehaviorSubject.create(0);
 
+    public void incrementScore(final int incrementBy) {
+        score.onNext(currentScore() + incrementBy);
     }
 
     public void decrementScore(final int decrementBy) {
-
+        score.onNext(currentScore() - decrementBy);
     }
 
     public int currentScore() {
-        return 0;
+        return score.getValue();
     }
 
     public Observable<Integer> scoreObservable() {
-        return null;
+        return score.asObservable();
     }
 
 }
