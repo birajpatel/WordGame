f683886bce2c9d12e29eed848858ca918714608b
Biraj Patel <biraj.d.patel@gmail.com>
2016-09-13 21:42:30 +0530

    Implemented HighscoreRepository

--- app/src/main/java/com/birin/wordgame/core/score/HighscoreRepository.java
+++ app/src/main/java/com/birin/wordgame/core/score/HighscoreRepository.java
@@ -3,28 +3,34 @@ package com.birin.wordgame.core.score;
 import com.birin.wordgame.core.utils.KeyValueDiskStore;
 
 import rx.Observable;
+import rx.subjects.BehaviorSubject;
 
 /**
  Created by Biraj on 9/13/16.
  */
 public class HighscoreRepository {
 
+    public static final String KEY_HIGHSCORE = "high_score";
+
     private KeyValueDiskStore keyValueDiskStore;
+    private BehaviorSubject<Integer> highscore = BehaviorSubject.create();
 
     public HighscoreRepository(KeyValueDiskStore keyValueDiskStore) {
         this.keyValueDiskStore = keyValueDiskStore;
+        this.highscore.onNext(keyValueDiskStore.readInt(KEY_HIGHSCORE, 0));
     }
 
     public void updateHighscore(int newScore) {
-
+        keyValueDiskStore.writeInt(KEY_HIGHSCORE, newScore);
+        highscore.onNext(newScore);
     }
 
     public int currentHighscore() {
-        return 0;
+        return highscore.getValue();
     }
 
     public Observable<Integer> highscoreObservable() {
-        return null;
+        return highscore.asObservable();
     }
 
 }
